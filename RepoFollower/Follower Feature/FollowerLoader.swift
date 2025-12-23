8c67f1a2f1f33ceed126709359beaaac6deaec04
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-12-23 16:48:45 +0700

    Change enum result to be Swift.Result

--- RepoFollower/Follower Feature/FollowerLoader.swift
+++ RepoFollower/Follower Feature/FollowerLoader.swift
@@ -5,11 +5,8 @@
 //  Created by Denny Arfansyah on 23/12/2025.
 //
 
-enum LoadFollowerResult {
-    case success([FollowerItem])
-    case error(Error)
-}
-
 protocol FollowerLoader {
-    func load(completion: @escaping (LoadFollowerResult) -> Void)
+    typealias Result = Swift.Result<[FollowerItem], Error>
+    
+    func load(completion: @escaping (Result) -> Void)
 }
