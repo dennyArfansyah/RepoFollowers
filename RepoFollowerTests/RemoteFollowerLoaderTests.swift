14ad80ba137b169926a439681a72a2b5a9a6a982
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-12-23 17:01:40 +0700

    Request data from URL on `load` using Singleton

--- RepoFollowerTests/RemoteFollowerLoaderTests.swift
+++ RepoFollowerTests/RemoteFollowerLoaderTests.swift
@@ -8,10 +8,14 @@
 import XCTest
 
 class RemoteFollowerLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")!
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
     var requestedURL: URL?
 }
 
@@ -19,10 +23,21 @@ class HTTPClient {
 final class RemoteFollowerLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFollowerLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        // Arrange
+        let client = HTTPClient.shared
+        let sut = RemoteFollowerLoader()
         
+        // act
+        sut.load()
+        
+        // Assert
+        XCTAssertNotNil(client.requestedURL)
     }
 }
