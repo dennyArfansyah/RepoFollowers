478bf60c9adf23dfba8d764175571c55030bbb4f
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-12-23 17:43:11 +0700

    Move `RemoteFollowerLoader` to production

--- RepoFollowerTests/RemoteFollowerLoaderTests.swift
+++ RepoFollowerTests/RemoteFollowerLoaderTests.swift
@@ -6,24 +6,7 @@
 //
 
 import XCTest
-
-class RemoteFollowerLoader {
-    let url: URL
-    let client: HTTPClient
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import RepoFollower
 
 final class RemoteFollowerLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
