b12419da127b9f711a645882456f6b7f4a6dda30
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-12-23 11:12:51 +0700

    Add architecture Image and follower item model

---
+++ FeedEssentialClone/FollowerItem.swift
@@ -0,0 +1,15 @@
+//
+//  FollowerItem.swift
+//  FeedEssentialClone
+//
+//  Created by Denny Arfansyah on 23/12/2025.
+//
+
+import Foundation
+
+struct FollowerItem {
+    let id: Int
+    let login: String
+    let avatarURL: URL
+    let reposURL: URL
+}
