9cd28190fcc70edb1a2c4d3706c5454fe513103c
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-12-23 17:14:48 +0700

    Make `HTTPClient` as protocol

--- RepoFollowerTests/RemoteFollowerLoaderTests.swift
+++ RepoFollowerTests/RemoteFollowerLoaderTests.swift
@@ -8,21 +8,25 @@
 import XCTest
 
 class RemoteFollowerLoader {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load() {
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
-class HTTPClient {
-    static var shared = HTTPClient()
-    
-    func get(from url: URL) {}
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
-    override func get(from url: URL) {
+    func get(from url: URL) {
         requestedURL = url
     }
 }
@@ -31,8 +35,7 @@ final class RemoteFollowerLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        _ = RemoteFollowerLoader()
+        _ = RemoteFollowerLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
     }
@@ -40,8 +43,7 @@ final class RemoteFollowerLoaderTests: XCTestCase {
     func test_load_requestDataFromURL() {
         // Arrange
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-         let sut = RemoteFollowerLoader()
+         let sut = RemoteFollowerLoader(client: client)
         
         // act
         sut.load()
